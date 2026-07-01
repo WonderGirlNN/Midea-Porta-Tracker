046b78965a96b40b1d1d43df0b6a7e7b3c946a2f
WonderGirlNN <lenalie21@protonmail.com>
2026-07-01 21:27:06 +0200

    Add 'toom' and 'otto' labels to _LABEL dictionary
    Added new labels for 'toom' and 'otto' to the label dictionary.

--- tracker/sources/baumarkt.py
+++ tracker/sources/baumarkt.py
@@ -19,7 +19,7 @@ from .jsonld import extract_products
 
 log = logging.getLogger(__name__)
 
-_LABEL = {"obi": "OBI", "bauhaus": "BAUHAUS", "hornbach": "Hornbach"}
+_LABEL = {"obi": "OBI", "bauhaus": "BAUHAUS", "hornbach": "Hornbach", "toom": "TOOM", "otto": "OTTO"}
 
 
 def _html(url: str) -> str | None:
